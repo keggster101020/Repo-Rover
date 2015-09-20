17a7d64424e6dcba0cf187b0fe5d5c54c48b6d8c
Keegan Shudy <shudyk@aldenv149.allegheny.edu>
2015-09-20 17:20:30 -0400

    starting to add running git status in the git directory

--- src/findGitDir.py
+++ src/findGitDir.py
@@ -1,5 +1,13 @@
 import os
+import subprocess
 
+"""
+This method will get all of the git repos listed under the given 
+directory
+
+param: directory path
+retuns: list
+"""
 def find_git_repos(dir):
     repos = []
     for (path, dirs, files) in os.walk(dir):
@@ -7,12 +15,30 @@ def find_git_repos(dir):
             if directory == ".git":
                 repos.append(path)
     return repos
+
+"""
+This will run git status for all the git repos
+and compile a list of repo dirs that have
+any staged commits
+
+param: list
+return: list
+"""
+def git_status(repos):
+	for repo in repos:
+		os.chdir(repo)
+		status = subprocess.check_output(['git', 'status'])
+		print status
+		
 				
 def main():
 	print "Enter the full directory you want to scan: "
 	initial_directory = raw_input()
-	print find_git_repos(initial_directory)
+	repos = find_git_repos(initial_directory)
+	print repos
+	print "\n %s" % repos[0] 
+	git_status([repos[0]])
 	
-	
+
 	
 if  __name__ =='__main__':main()
