2d68b6b4d4191ac0ac63d52151bdccd7b9f066a9
Keegan Shudy <shudyk@aldenv105.allegheny.edu>
2015-09-20 21:14:39 -0400

    added file tracking for the specific messages :)

--- src/findGitDir.py
+++ src/findGitDir.py
@@ -30,16 +30,18 @@ def git_status(repos):
 	for repo in repos:
 		os.chdir(repo)
 		status = subprocess.check_output(['git', 'status'])
-		if has_issues(status):
-			print [False, repo, status] #debug output
-			#run_report(repo,[status])
-		print [True, repo] #also debug
+		issues = has_issues(status)
+		if issues[0]:
+			print issues #debug output
+		else:
+			print [True, repo] #also debug
 """
 check the git status message to see if there are changes 
 that haven't been added or committed or pushed
 """
 def has_issues(message):
 	issues = []
+	bool_issues = False
 	changes_staged = "changes not staged for commit"
 	changes_committed = "changes to be committed"
 	changes_pushed = "your branch is ahead of"
@@ -47,12 +49,33 @@ def has_issues(message):
 	changes_not_committed = changes_committed in message.lower()
 	committed_not_pushed = changes_pushed in message.lower()
 	changes_not_added = changes_staged in message.lower()
-	#if changes_not_added:
-		#get_file(message)
+	if changes_not_added:
+		bool_issues = True
+		file_name = get_file(message)
+		issues.append("changes not staged for commit : %s" % file_name)
+	if committed_not_pushed:
+		bool_issues = True
+		file_name = get_file(message)
+		issues.append("You have a commit that you have not pushed : %s" % file_name)
+	if changes_not_committed:
+		bool_issues = True
+		file_name = get_file(message)
+		issues.append("Changes to be committed %s" % file_name)
 	
-	return changes_not_committed or committed_not_pushed or changes_not_added #this will be removed 
-
-#def get_file(message):
+	return [bool_issues, issues]
+	
+	#return changes_not_committed or committed_not_pushed or changes_not_added #this will be removed 
+"""
+Gets the file that is being staged in git
+"""
+def get_file(message):
+	file_index = []
+	split = message.split()
+	count_mod = split.count('modified:')
+	
+	for i in range(0, count_mod):
+		file_index.append(split[split.index('modified:',split.index('modified:')) + 1])
+	return file_index
 	
 				
 def main():
