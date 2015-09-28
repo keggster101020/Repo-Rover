f20b50586596c61eacf68679360731f7662486aa
Cody Kinneer <kinneerc@allegheny.edu>
2015-09-28 18:22:37 -0400

    renamed files to fix namespace issues

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 version = '1.0'
 
-setup(name='reporover',
+setup(name='repo_rover',
       version=version,
       description='Take back control of your unruly git repositories.',
       author='Cody Kinneer, Keegan Shudy',
       author_email='kinneerc@allegheny.edu',
       url='https://github.com/keggster101020/Repo-Rover',
-      packages=['reporover'],
-      scripts=['reporover'],
+      packages=['repo_rover'],
+      package_data={'repo_rover':['*.html','*.css']},
+      scripts=['repo_rover/reporover'],
       classifiers=['Development Status :: 4 - Beta',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
